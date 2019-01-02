8ccc2400105503e4cb3e729198c97db7a8d7a9f5
Dan Michael O. Heggø <danmichaelo@gmail.com>
2019-01-03 00:25:40 +0100

    Add authority file for places, reformat for easier reconciliation

--- Makefile
+++ Makefile
@@ -2,10 +2,10 @@ LOCALE:="no_NO"
 .PHONY: all
 all: build/hansteen.marc21.xml
 
-src/hansteen.json: src/person_autoriteter.json scripts/authorize.py
+src/hansteen.json: src/places.json src/agents.json scripts/authorize.py
 	LC_ALL=$(LOCALE) python scripts/authorize.py
 
-build/hansteen.marc21.xml: src/hansteen.json src/person_autoriteter.json scripts/json2marc.py
+build/hansteen.marc21.xml: src/hansteen.json scripts/json2marc.py
 	LC_ALL=$(LOCALE) python scripts/json2marc.py src/hansteen.json > build/hansteen.marc21.xml
 
 clean:
--- scripts/authorize.py
+++ scripts/authorize.py
@@ -2,43 +2,62 @@ import json
 import re
 import sys
 import csv
+from collections import OrderedDict
 
-filename = 'src/hansteen.json'
-aut_file = 'src/person_autoriteter.json'
-
-
-# auts = {}
-# with open('src/openrefine_reconcile.tsv') as fp:
-#     for row in csv.DictReader(fp, delimiter='\t'):
-#         auts[row['id']] = row
+main_file = 'src/hansteen.json'
+agents_file = 'src/agents.json'
+places_file = 'src/places.json'
 
 
-with open(aut_file, 'r') as fp:
-    authorities = json.load(fp)
+def read_aut_file(filename):
+    with open(filename, 'r') as fp:
+        obj = json.load(fp)
+        obj = {x['name']: x for x in obj}
+    return obj
 
 
-labels = set()
-with open(filename, 'r') as fp:
+def write_aut_file(obj, filename):
+    with open(filename, 'w') as fp:
+        canonical_rep = sorted(obj.values(), key=lambda x: x['name'])
+        canonical_rep = [OrderedDict(sorted(x.items())) for x in canonical_rep]
+        json.dump(canonical_rep, fp, indent=2, ensure_ascii=False)
+
+
+agents = read_aut_file(agents_file)
+places = read_aut_file(places_file)
+
+with open(main_file, 'r') as fp:
     data = json.load(fp)
     for letter in data:
+
+        for place in letter['descriptive']['places']:
+            aut = places.get(place['place'])
+            if aut is None:
+                places[place['place']] = {
+                    'name': place['place'],
+                    'country': place['country'],
+                    'wikidata_id': None,
+                }
+            elif aut.get('wikidata_id') is not None:
+                place['wikidata_id'] = aut['wikidata_id']
+
         for agent_type, agent in letter['descriptive']['agents'].items():
+            aut = agents.get(agent['name'])
+            if aut is None:
+                agents[agent['name']] = {
+                    'name': agent['name'],
+                    'wikidata_id': None,
+                }
+            else:
+                if aut.get('bibsys_id') is not None:
+                    agent['bibsys_id'] = aut['bibsys_id']
+                if aut.get('wikidata_id') is not None:
+                    agent['wikidata_id'] = aut['wikidata_id']
+                if aut.get('bibsys_name') is not None:
+                    agent['aut_name'] = aut['bibsys_name']
 
-            # if agent_type == 'correspondent' and auts.get(letter['id']):
-            #     aut = auts.get(letter['id'])
-            #     agent['wikidata_id'] = aut['wd']
-            #     agent['aut_name'] = aut['name']
-
-            aut = authorities.get(agent['name'])
-            if aut is not None and aut.get('bibsys_id') is not None:
-                agent['bibsys_id'] = aut['bibsys_id']
-            if aut is not None and aut.get('wikidata_id') is not None:
-                agent['wikidata_id'] = aut['wikidata_id']
-            elif agent.get('wikidata_id'):
-                print('Oi:', agent.get('wikidata_id'))
-            if aut is not None and aut.get('bibsys_name') is not None:
-                agent['aut_name'] = aut['bibsys_name']
-
-
-
-with open(filename, 'w') as fp:
+with open(main_file, 'w') as fp:
     json.dump(data, fp, indent=2, ensure_ascii=False)
+
+write_aut_file(places, places_file)
+write_aut_file(agents, agents_file)
